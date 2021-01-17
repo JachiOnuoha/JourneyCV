36d38fcd9b5d0cb0a552692ab73fb3f592d05fdc
Jachimike Onuoha <jon226@g.uky.edu>
2021-01-16 20:39:29 -0500

    Changed it to from cv2 to cv

--- main/main.py
+++ main/main.py
@@ -1,17 +1,17 @@
 import os
-import cv2
+import cv2 as cv
 
 # Get the path of the cascade used for detection
-cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
+cv2_base_dir = os.path.dirname(os.path.abspath(cv.__file__))
 print(cv2_base_dir)
 cascadePath = os.path.join(
     cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
 
 # Create cascade object
-haar_model = cv2.CascadeClassifier(cascadePath)
+haar_model = cv.CascadeClassifier(cascadePath)
 
 # Select the webcam to be used for capture
-vidCapture = cv2.VideoCapture(0)
+vidCapture = cv.VideoCapture(0)
 
 while True:
     # Read the video frame by frame everyloop and also check for the return value if the
@@ -19,7 +19,7 @@ while True:
     ret, frame = vidCapture.read()
 
     # Convert each frame to grayscale
-    grayVid = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayVid = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     # Detect faces in the images
     faces = haar_model.detectMultiScale(
@@ -27,19 +27,19 @@ while True:
         scaleFactor=1.1,
         # Increase this number for a better quality of detection
         minNeighbors=5,
-        flags=cv2.CASCADE_SCALE_IMAGE
+        flags=cv.CASCADE_SCALE_IMAGE
     )
 
     # Draw a rectangle around any face found using the returned coordinates
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
 
     # Show every frame
-    cv2.imshow("CompSight", frame)
+    cv.imshow("CompSight", frame)
 
     # Stop process if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 vidCapture.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
