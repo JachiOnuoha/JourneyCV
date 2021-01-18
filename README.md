746669bafe281ca351994a464e71e45c5c38880e
Jachimike Onuoha <jon226@g.uky.edu>
2021-01-18 18:48:26 -0500

    Added readme and cleaned up & renamed match template

---
+++ README.md
@@ -0,0 +1,2 @@
+# OpenCV Practice Repo
+In this repository, I practice image transformation, object detction and various other computer vision techniques to improve my skills
--- main/objdetect-mtchtemplt.py
+++ main/objdetect-mtchtemplt.py
@@ -1,3 +1,5 @@
+# Practice using match template
+
 import os
 import cv2 as cv
 import numpy as np
@@ -10,22 +12,20 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 templateImg = cv.imread("Icarus2.jpeg", cv.IMREAD_UNCHANGED)
 sourceImg = cv.imread("Icarus_and_Sunia.jpeg", cv.IMREAD_UNCHANGED)
 
-result = cv.matchTemplate(sourceImg, templateImg, cv.TM_CCOEFF_NORMED)
-minval, maxval, minLoc, maxLoc = cv.minMaxLoc(result)
-
-print(maxLoc)
-print(maxval)
-
+# Get the width and length of the source image and template
 template_w = templateImg.shape[1]
 template_h = templateImg.shape[0]
 
+# Apply match template to the images
+result = cv.matchTemplate(sourceImg, templateImg, cv.TM_CCOEFF_NORMED)
+
+# Get the best and worst maatches and their coordinates(top left corners)
+minval, maxval, minLoc, maxLoc = cv.minMaxLoc(result)
+
+# Create the rectangles dimensions
 bottom = (maxLoc[0]+template_w, maxLoc[1]+template_h)
 
-# cv.imshow("Result", result)
-# cv.waitKey()
-# cv.imshow("Result", sourceImg)
-# cv.waitKey()
+# Draw a rectangle on the best match
 cv.rectangle(sourceImg, maxLoc, bottom, (0, 255, 0))
 cv.imshow("Results", sourceImg)
 cv.waitKey()
-# print(maxLoc)
