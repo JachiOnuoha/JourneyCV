06e2794d440ae6c1af62a88d988a09f1a6e6be0d
Jachimike Onuoha <jon226@g.uky.edu>
2021-01-17 22:23:34 -0500

    Object detectiojn using template matching

--- main/objdetect.py
+++ main/objdetect.py
@@ -1,2 +1,31 @@
 import os
 import cv2 as cv
+import numpy as np
+
+
+# Access the fle in this file path
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# Load up the source image and image to be searched
+templateImg = cv.imread("Icarus2.jpeg", cv.IMREAD_UNCHANGED)
+sourceImg = cv.imread("Icarus_and_Sunia.jpeg", cv.IMREAD_UNCHANGED)
+
+result = cv.matchTemplate(sourceImg, templateImg, cv.TM_CCOEFF_NORMED)
+minval, maxval, minLoc, maxLoc = cv.minMaxLoc(result)
+
+print(maxLoc)
+print(maxval)
+
+template_w = templateImg.shape[1]
+template_h = templateImg.shape[0]
+
+bottom = (maxLoc[0]+template_w, maxLoc[1]+template_h)
+
+# cv.imshow("Result", result)
+# cv.waitKey()
+# cv.imshow("Result", sourceImg)
+# cv.waitKey()
+cv.rectangle(sourceImg, maxLoc, bottom, (0, 255, 0))
+cv.imshow("Results", sourceImg)
+cv.waitKey()
+# print(maxLoc)
