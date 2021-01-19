e9feae0ec4b5d81f496fc6a7f2a62436448aa435
Jachimike Onuoha <jon226@g.uky.edu>
2021-01-18 21:08:57 -0500

    Completed template matching with groupRectangles

--- main/objdetect-mtchtemplt.py
+++ main/objdetect-mtchtemplt.py
@@ -5,12 +5,13 @@ import cv2 as cv
 import numpy as np
 
 
-# Access the fle in this file path
+# Access the file in this file path
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Load up the source image and image to be searched
-templateImg = cv.imread("Icarus2.jpeg", cv.IMREAD_UNCHANGED)
-sourceImg = cv.imread("Icarus_and_Sunia.jpeg", cv.IMREAD_UNCHANGED)
+templateImg = cv.imread("house2.jpeg", cv.IMREAD_UNCHANGED)
+sourceImg = cv.imread("farm2.jpeg", cv.IMREAD_UNCHANGED)  # 960 x 540
+
 
 # Get the width and length of the source image and template
 template_w = templateImg.shape[1]
@@ -22,10 +23,38 @@ result = cv.matchTemplate(sourceImg, templateImg, cv.TM_CCOEFF_NORMED)
 # Get the best and worst maatches and their coordinates(top left corners)
 minval, maxval, minLoc, maxLoc = cv.minMaxLoc(result)
 
-# Create the rectangles dimensions
-bottom = (maxLoc[0]+template_w, maxLoc[1]+template_h)
+# Set trhreshold for what is considered a good match
+threshold = 0.40
 
-# Draw a rectangle on the best match
-cv.rectangle(sourceImg, maxLoc, bottom, (0, 255, 0))
-cv.imshow("Results", sourceImg)
-cv.waitKey()
+# Save coordinates that meet threshold requirement
+locations = np.where(result >= threshold)
+locations = list(zip(*locations[::-1]))  # Reverse and combine to (x,y) format
+
+# Create rectangles which consist of top left coordnate and the width and height of the template
+# i.e (x,y,w,h)
+rectangles = []
+for loc in locations:
+    rect = [int(loc[0]), int(loc[1]), int(template_w), int(template_h)]
+    rectangles.append(rect)
+
+print("Rectangles before grouping:\n {}\n".format(rectangles))
+
+# Use groupRectangles to group overlapping rectangles which do not differ by more
+# than 0.5 apart and require there to be atleast 1 overlap
+rectangles, weights = cv.groupRectangles(rectangles, 1, eps=0.5)
+print("Rectangles after grouping:\n {}\n".format(rectangles))
+
+
+if len(rectangles):
+
+    # Iterate over rectangle elements
+    for (x, y, w, h) in rectangles:
+        coord = (x, y)
+        dimensions = (x+w, y+h)
+
+        # Draw a green rectangle on all the matching coordinates
+        cv.rectangle(sourceImg, coord, dimensions, (0, 255, 0))
+
+    # Display the source image with the best match identified
+    cv.imshow("Results", sourceImg)
+    cv.waitKey()
